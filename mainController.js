818cefd51b860a50566c5cb7aed530064d48e43d
raguiar2 <raguiar2@stanford.edu>
2017-06-25 01:17:34 -0700

    Added some text fields

--- mainController.js
+++ mainController.js
@@ -1,27 +1,27 @@
 'use strict';
 
 //included theme
-var nameReminderApp = angular.module('nameReminderApp', ['ngRoute', 'ngMaterial', 'ngResource']).config(function($mdThemingProvider) {
+var personalWebsite = angular.module('personalWebsite', ['ngRoute', 'ngMaterial', 'ngResource']).config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
         .primaryPalette('light-green')
         .accentPalette('orange');
 });
 
-nameReminderApp.config(['$routeProvider', '$resourceProvider',
-    function($routeProvider) {
-        $routeProvider.
-        when('/', {
-            title: 'Home',
-            templateUrl: 'index.html',
-            controller: 'MainController.js'
-        }).
-        otherwise({
-            redirectTo: '/'
-        });
-    }
-]);
+// personalWebsite.config(['$routeProvider', '$resourceProvider',
+//     function($routeProvider) {
+//         $routeProvider.
+//         when('/', {
+//             title: 'Home',
+//             templateUrl: 'index.html',
+//             controller: 'MainController.js'
+//         }).
+//         otherwise({
+//             redirectTo: '/'
+//         });
+//     }
+// ]);
 
-nameReminderApp.controller('MainController', ['$scope', '$rootScope', '$location', '$routeParams', '$resource', '$http',
+personalWebsite.controller('MainController', ['$scope', '$rootScope', '$location', '$routeParams', '$resource', '$http',
 	function($scope, $rootScope, $location, $routeParams, $resource, $http) {
         console.log("HERE")
 	}]);
